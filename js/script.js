003cbba742034d643d91abef2549a19933c8cc72
Natalia <natalia_bochenek@hotmail.com>
2021-03-12 20:30:58 +0100

    correct a bug in line 35 (connect likes & index)

--- js/script.js
+++ js/script.js
@@ -2,12 +2,7 @@
 let moviesData = JSON.parse(movies);
 //console.log(moviesData);
 
-let newArray = []; // setting an empty array=[]
-
 for (let movie = 0 ; movie < moviesData.length; movie++) {
-  let like = moviesData[movie].likes; //stores the number
-  //console.log(like)
-
   document.getElementById('printMovies').innerHTML +=
 
   //dynamic section of the website (displaying movies)
@@ -37,11 +32,31 @@ for (let movie = 0 ; movie < moviesData.length; movie++) {
   //iterate through each buttons and apply a "click" event for each
     //btn: the current button; index: i in the array (btns).
   btns.forEach((btn, index) => {
-    btnsClicksCountArray[index] = `${like}`; //start the second array (btnsClicksCountArray) at the index: 0 
+    btnsClicksCountArray[index] = `${moviesData[index].likes}`; //start the second array (btnsClicksCountArray) at the index: 0 
     btn.addEventListener('click', () => btn.innerHTML = ++btnsClicksCountArray[index]); //add event listener for "click" and increment the value stored in btnsClicksCountArray with its index i; and write it in the button
   });
 }
 
 
 
+/*   this solution works!!!!!
+let btns = [...document.getElementsByClassName(`clickButton`)], //*see comment below
+  btnsClicksCountArray = new Array(btns.length);
+
+  //iterate through each buttons and apply a "click" event for each
+    //btn: the current button; index: i in the array (btns).
+  btns.forEach((btn, index) => {
+    btnsClicksCountArray[index] = `${like}`; //start the second array (btnsClicksCountArray) at the index: 0 
+    btn.addEventListener('click', () => btn.innerHTML = ++btnsClicksCountArray[index]); //add event listener for "click" and increment the value stored in btnsClicksCountArray with its index i; and write it in the button
+  });
+   */
+
+
+
 //* "...document." The three dots in JS are spread/rest operator. This  syntax allows an expression to be expanded in places where multiple arguments are expected. The rest parameter syntax is used for functions with variable number of arguments. The spread rest operator for arrays was introduced in ES6.
+
+
+
+
+//SORT BUTTON - sort movies according to their current like property status (e.g. show on the top of the list the movie whose like property is currently the highest one).
+
