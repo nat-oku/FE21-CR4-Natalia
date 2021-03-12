59ce571d9a5f716031a0f48ca1eecd1b2197bb4e
Natalia <natalia_bochenek@hotmail.com>
2021-03-12 23:04:43 +0100

    add btn function for dynamic section

--- js/script.js
+++ js/script.js
@@ -25,17 +25,23 @@ for (let movie = 0 ; movie < moviesData.length; movie++) {
     </div>
   </div>
   `
-  //create a new array to store the click counters for each button
-  let btns = [...document.getElementsByClassName(`clickButton`)], //*see comment below
-  btnsClicksCountArray = new Array(btns.length);
-
-  //iterate through each buttons and apply a "click" event for each
-    //btn: the current button; index: i in the array (btns).
-  btns.forEach((btn, index) => {
-    btnsClicksCountArray[index] = `${moviesData[index].likes}`; //start the second array (btnsClicksCountArray) at the index: 0 
-    btn.addEventListener('click', () => btn.innerHTML = ++btnsClicksCountArray[index]); //add event listener for "click" and increment the value stored in btnsClicksCountArray with its index i; and write it in the button
-  });
 }
+
+//create a new array to store the click counters for each button
+let btns = [...document.getElementsByClassName(`clickButton`)]; //*see comment below
+btnsClicksCountArray = new Array(btns.length);
+
+//iterate through each buttons and apply a "click" event for each
+  //btn: the current button; index: i in the array (btns).
+btns.forEach((btn, index) => {
+  btnsClicksCountArray[index] = `${moviesData[index].likes}`; //start the second array (btnsClicksCountArray) at the index: 0 
+  btn.addEventListener('click', () => btn.innerHTML = ++btnsClicksCountArray[index]); //add event listener for "click" and increment the value stored in btnsClicksCountArray with its index; and write it in the button
+});
+
+console.table(moviesData.sort())
+
+
+
 
 //* "...document." The three dots in JS are spread/rest operator. This  syntax allows an expression to be expanded in places where multiple arguments are expected. The rest parameter syntax is used for functions with variable number of arguments. The spread rest operator for arrays was introduced in ES6.
 
@@ -44,3 +50,11 @@ for (let movie = 0 ; movie < moviesData.length; movie++) {
 
 //SORT BUTTON - sort movies according to their current like property status (e.g. show on the top of the list the movie whose like property is currently the highest one).
 
+//erstele einen button, der einen Array hat; sobald der Button geklickt wird, sortieren sich die filme nach der Anzahl der klicks
+
+function sortList() {
+
+  
+}
+
+document.getElementById("sortButton").addEventListener("click",sortList);
