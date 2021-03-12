e455b2a6b888d69846563394e85dae5a73c08e58
Natalia <natalia_bochenek@hotmail.com>
2021-03-12 19:44:32 +0100

    add solution for button clicks & counter

--- js/script.js
+++ js/script.js
@@ -2,8 +2,15 @@
 let moviesData = JSON.parse(movies);
 //console.log(moviesData);
 
-for (movie in moviesData) {
+let newArray = []; // setting an empty array=[]
+
+for (let movie = 0 ; movie < moviesData.length; movie++) {
+  let like = moviesData[movie].likes; //stores the number
+  //console.log(like)
+
   document.getElementById('printMovies').innerHTML +=
+
+  //dynamic section of the website (displaying movies)
   `
   <div class="card mb-3 cardWidth alignCenter">
     <div class="row g-0 bg-success">
@@ -16,11 +23,21 @@ for (movie in moviesData) {
           <p class="card-text">${moviesData[movie].description}</p>
           <div class="d-flex justify-content-end" >
             <p class="card-text m-2">Like <i class="fas fa-thumbs-up"></i></p>
-            <p class="likeButton m-2">${moviesData[movie].likes}</p>
+            <button class="m-2 clickButton" id="likeButton">${moviesData[movie].likes}</button>
           </div>
         </div>
       </div>
     </div>
   </div>
   `
+  //create a new array to store the click counters for each button
+  let btns = [...document.getElementsByClassName(`clickButton`)],
+  btnsClicksCountArray = new Array(btns.length);
+
+  //iterate through each buttons and apply a "click" event for each
+    //btn: the current button; index: i in the array (btns).
+  btns.forEach((btn, index) => {
+    btnsClicksCountArray[index] = 0; //initialize the second array (btnsClicksCountArray) at the index: 0  
+    btn.addEventListener('click', () => btn.innerHTML = ++btnsClicksCountArray[index]); //add event listener for "click" and increment the value stored in btnsClicksCountArray with its index i; and write it in the button
+  });
 }
