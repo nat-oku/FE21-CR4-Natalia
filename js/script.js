d52d57de30f1ce5db131d89933a06caa2618f6ff
Natalia <natalia_bochenek@hotmail.com>
2021-03-13 11:02:40 +0100

    add 50% solution for bonus exersice

--- js/script.js
+++ js/script.js
@@ -7,29 +7,31 @@ for (let movie = 0 ; movie < moviesData.length; movie++) {
 
   //dynamic section of the website (displaying movies)
   `
-  <div class="card mb-3 cardWidth alignCenter">
-    <div class="row g-0 bg-success">
-      <div class="col-md-4 d-flex align-items-center">
-        <img src="${moviesData[movie].image}" class="img-thumbnail myImage bg-dark" alt="Movie Image">
-      </div>
-      <div class="col-md-8">
-        <div class="card-body">
-          <h5 class="card-title">${moviesData[movie].movieName}</h5>
-          <p class="card-text">${moviesData[movie].description}</p>
-          <div class="d-flex justify-content-end" >
-            <p class="card-text m-2">Like <i class="fas fa-thumbs-up"></i></p>
-            <button class="m-2 clickButton" id="likeButton">${moviesData[movie].likes}</button>
+  <li class="movieListItem">
+    <div class="card mb-3 cardWidth alignCenter">
+      <div class="row g-0 bg-success">
+        <div class="col-md-4 d-flex align-items-center">
+          <img src="${moviesData[movie].image}" class="img-thumbnail myImage bg-dark" alt="Movie Image">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">${moviesData[movie].movieName}</h5>
+            <p class="card-text">${moviesData[movie].description}</p>
+            <div class="d-flex justify-content-end" >
+              <p class="card-text m-2">Like <i class="fas fa-thumbs-up"></i></p>
+              <button class="m-2 clickButton" id="likeButton">${moviesData[movie].likes}</button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </li>
   `
 }
 
 //create a new array to store the click counters for each button
 let btns = [...document.getElementsByClassName(`clickButton`)]; //*see comment below
-btnsClicksCountArray = new Array(btns.length);
+btnsClicksCountArray = new Array(btns.length); // saves the new click count
 
 //iterate through each buttons and apply a "click" event for each
   //btn: the current button; index: i in the array (btns).
@@ -37,8 +39,7 @@ btns.forEach((btn, index) => {
   btnsClicksCountArray[index] = `${moviesData[index].likes}`; //start the second array (btnsClicksCountArray) at the index: 0 
   btn.addEventListener('click', () => btn.innerHTML = ++btnsClicksCountArray[index]); //add event listener for "click" and increment the value stored in btnsClicksCountArray with its index; and write it in the button
 });
-
-console.table(moviesData.sort())
+console.log(btnsClicksCountArray)
 
 
 
@@ -48,13 +49,15 @@ console.table(moviesData.sort())
 
 
 
-//SORT BUTTON - sort movies according to their current like property status (e.g. show on the top of the list the movie whose like property is currently the highest one).
+  //SORT BUTTON
+// function - change the color of button's background
+function clickButton() {
+  document.getElementById("sortButton").style.background = "red";
+  //document.getElementsByClassName("movieListItem")[0].style.color = "yellow";
 
-//erstele einen button, der einen Array hat; sobald der Button geklickt wird, sortieren sich die filme nach der Anzahl der klicks
-
-function sortList() {
-
-  
+  return btnsClicksCountArray.sort(function sortList(a,b) { //sort list by numeric values - console.log to check
+    return b - a;
+  })
 }
 
-document.getElementById("sortButton").addEventListener("click",sortList);
+document.getElementById('sortButton').addEventListener('click', clickButton, false);
